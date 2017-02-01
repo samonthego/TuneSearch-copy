975fb78e4dba59ad7b9e4a9b1348c32f9a993b00
samonthego <samonthego@r176-24-171-108.wccnet.edu>
2017-01-31 19:34:39 -0500

    working on phone.

--- inetwork/ViewController.swift
+++ inetwork/ViewController.swift
@@ -10,14 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let hostName = "www.moveablebytes.com"
+    var reachability : Reachability?
+    
+    @IBOutlet var networkStatusLabel :UILabel!
+    
+    //MARK: - Reachability Methods
+    
+    func setupReacability(hostName: String)  {
+        reachability = Reachability(hostname: hostName)
+        reachability!.whenReachable = { reachability in
+            DispatchQueue.main.async {
+                self.updateLabel(reachable: true, reachability: reachability)
+            }
+            
+        }
+        reachability!.whenUnreachable = {reachability in
+            self.updateLabel(reachable: false, reachability: reachability)        }
+    }
+    
+    func startReachability() {
+        do{
+            try reachability!.startNotifier()
+        }catch{
+            networkStatusLabel.text = "Unable to Start Notifier"
+            networkStatusLabel.textColor = .red
+            return
+        }
+    }
+    
+    func updateLabel(reachable: Bool, reachability: Reachability){
+        if reachable {
+            if reachability.isReachableViaWiFi{
+            networkStatusLabel.textColor = .green}
+            else {
+            networkStatusLabel.textColor = .blue
+            }
+        }else{
+            networkStatusLabel.textColor = .red
+        }
+        networkStatusLabel.text = reachability.currentReachabilityString
+    }
+    
+    //Mark: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupReacability(hostName: hostName)
+        startReachability()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
 
